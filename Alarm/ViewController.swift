b154b6b6ea90c7854e157a92549d4eaf4e18e622
Yichuan Wang <w369371@gmail.com>
2020-01-23 23:38:59 +0800

    initial commit

--- Alarm/ViewController.swift
+++ Alarm/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLbel: UILabel!
+    
+    private let log = Log()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let dateformatter = DateFormatter()
+
+        dateformatter.dateFormat = "HH:mm:ss"// 自定义时间格式
+
+        let time = dateformatter.string(from: Date())
+        myLbel.text = time
     }
-
-
+    
+    func consumeLog(log: String) {
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "HH:mm:ss"// 自定义时间格式
+        myLbel.text = dateformatter.string(from: Date())
+        
+       }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] _ in
+            self?.log.requestLog{ data in
+                self?.consumeLog(log: data)
+            }
+        }
+    }
+}
+class Log {
+    func requestLog(consume: (_ log: String) -> Void) {
+        let slog = "Data from wherever"
+        consume(slog)
+    }
 }
 
