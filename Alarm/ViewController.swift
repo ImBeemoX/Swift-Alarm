2ade68abc62bc5bcc534391b71202c234b1dfd66
Yichuan Wang <w369371@gmail.com>
2020-01-23 23:08:24 +0800

    Initial Commit

---
+++ Alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarm
+//
+//  Created by Yichuan Wang on 23/1/20.
+//  Copyright © 2020 Wang Yichuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
